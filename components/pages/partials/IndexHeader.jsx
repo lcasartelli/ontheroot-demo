0b5431859ec92bd9230f868cc11830927741e197
Luca Casartelli <lc.casartelli@gmail.com>
2015-04-26 12:18:15 +0200

    fix merge error

--- components/pages/partials/IndexHeader.jsx
+++ components/pages/partials/IndexHeader.jsx
@@ -32,8 +32,10 @@ module.exports = function (data) {
           <div className="text-center">
             <div className="spacer-150"></div>
             <div className="spacer-80"></div>
+
             <img src='./assets/img/ontheroot-white.png' style={{ width: "300px" }}/>
             <h3>Order your favourite food, the 1st delivery is on us!</h3>
+
             <div className="spacer-150"></div>
             <div className="spacer-20"></div>
           </div>
