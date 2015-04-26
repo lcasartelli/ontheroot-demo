fc0614c7c045ecf453fe932dc4fe1dd749e0100b
Luca Casartelli <lc.casartelli@gmail.com>
2015-04-26 19:55:58 +0200

    fix add to cart label

--- components/elements/DishModal.jsx
+++ components/elements/DishModal.jsx
@@ -133,7 +133,7 @@ module.exports = function (treeData) {
               </div>
               <button className="pure-button pure-success" onClick={this.addToCart}>
                 <i className="fa fa-cart-plus"></i>
-                <span>Aggiungi al carrello</span>
+                <span>Add to cart</span>
               </button>
             </div>
           </div>
